3143c57e871cd8767e49d82f3971f8501ad4e5fd
NeonSpork <33250853+NeonSpork@users.noreply.github.com>
2018-05-13 21:15:46 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
+# MAKE SURE TO EDIT YOUR_FOLDER TO THE CORRECT DIRECTORY
 
 cd /
-cd home/pi/Desktop/weather
+cd home/pi/YOUR_FOLDER/weather_display
 python weather_display.py
 cd /
